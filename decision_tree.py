8f8e68886a70bd33ab199d855c852dcd8325dc9f
JShaw <93174861+JShawGit@users.noreply.github.com>
2022-04-21 01:35:24 -0500

    Add files via upload

--- decision_tree.py
+++ decision_tree.py
@@ -2,12 +2,18 @@ import numpy as np
 
 """ Decision Tree ======================================================================================================
     A supervised learning method.
+    
+    Good References:
+    https://engineering.purdue.edu/kak/Tutorials/DecisionTreeClassifiers.pdf
+    https://www.softwaretestinghelp.com/decision-tree-algorithm-examples-data-mining/
 """
 class DecisionTree:
     """ Init Tree -------------------------------------------------------------------------------------------------- """
-    def __init__(self, min_split=0, depth=0):
-        self.root = None
-        self.depth = depth
+    def __init__(self, min_split=3, depth=3):
+        self.dim       = 32*32*3
+        self.root      = None
+        self.depth     = depth
+        self.weights   = np.zeros((self.dim, 1))
         self.min_split = min_split
     # --- End Init Tree --- #
 
@@ -25,11 +31,11 @@ class DecisionTree:
     # --- End Create Node --- #
 
 
-    """ Create Tree ------------------------------------------------------------------------------------------------ """
-    def create_tree(self, training_data, current_depth):
+    """ Fit -------------------------------------------------------------------------------------------------------- """
+    def fit(self, training_data, current_depth):
         # Ref: https://www.youtube.com/watch?v=sgQAhG5Q7iY
-        x = training_data[0]
-        y = training_data[1]
+        x = training_data[:,:-1]
+        y = training_data[:,:-1]
         n_samples, n_features = np.shape(x)
 
         # split tree in the best way if possible
@@ -38,8 +44,8 @@ class DecisionTree:
 
             # if info gain is positive add onto tree
             if best_split["info_gain"] > 0:
-                false_node = self.create_tree(best_split["data_false"])
-                true_node  = self.create_tree(best_split["data_true"])
+                false_node = self.fit(best_split["data_false"])
+                true_node  = self.fit(best_split["data_true"])
                 return Node(
                     label=None,
                     feature_index=best_split["feature_index"],
@@ -55,7 +61,7 @@ class DecisionTree:
 
     """ Best Split ------------------------------------------------------------------------------------------------- """
     def best_split(self, training_data, n_samples, n_features):
-        max_info_gain = -float("inf")
+        max_gain = -float("inf")
         best_split = {
             "data_false":    None,
             "data_true":     None,
@@ -66,24 +72,51 @@ class DecisionTree:
 
         # for every feature
         for feature_index in range(n_features):
-            feature_values = [] # TODO
+            feature_values = training_data[:n_samples]
             thresholds = np.unique(feature_values)
 
             for t in thresholds:
                 data_left, data_right = self.split(training_data, feature_index, t)
                 if len(data_left) > 0 and len(data_right) > 0:
-                    return None # TODO
+
+                    y, l, r = training_data[:, -1], data_left[:, -1], data_right[:, -1]
+                    gain = self.info_gain(y, l, r)
+
+                    if gain > max_gain:
+                        best_split["data_false"]    = data_left
+                        best_split["data_true"]     = data_right
+                        best_split["feature_index"] = feature_index
+                        best_split["info_gain"]     = gain
+                        best_split["threshold"]     = t
+                        max_gain = gain
 
         return best_split
     # --- End Best Split --- #
 
 
-    """ Fit -------------------------------------------------------------------------------------------------------- """
-    def fit(self, training_data, epochs, batch_size):
-        assert training_data[0].shape[0] == len(training_data[1]), \
-            "Training data array length not equal to validation array!"
-        print("TODO")
-    # --- End Fit --- #
+    """ Info Gain -------------------------------------------------------------------------------------------------- """
+    def info_gain(self, parent, l, r):
+        w_l = len(l) / len(parent)
+        w_r = len(r) / len(parent)
+
+        def entropy(x):
+            labels = np.unique(x)
+            ent    = 0
+            for lab in labels:
+                pct = len(x[x==lab]) / len(x)
+                ent += -pct * np.log2((pct))
+            return ent
+
+        return entropy(parent) - (w_l * entropy(l) + w_r * entropy(r))
+    # --- End Info Gain --- #
+
+
+    """ Split ------------------------------------------------------------------------------------------------------ """
+    def split(self, data, index, thresh):
+        l = np.array([d for d in data if d[index] <= thresh])
+        r = np.array([d for d in data if d[index]  > thresh])
+        return l, r
+    # --- End Split --- #
 
 """ END DECISION TREE ////////////////////////////////////////////////////////////////////////////////////////////// """
 
@@ -112,7 +145,7 @@ class Node:
         else:
             self.is_leaf = True
             self.leaves = {
-                True:  None,\
+                True:  None,
                 False: None
             }
     # --- End Init Node --- #
@@ -126,3 +159,12 @@ class Node:
     # --- End Add Leaves --- #
 
 """ END NODE /////////////////////////////////////////////////////////////////////////////////////////////////////// """
+
+import read_dataset as r
+
+model = DecisionTree()
+x_train, x_test, y_train, y_test = r.read_dataset("airplane")
+data = np.concatenate((x_train, y_train), axis=0)
+
+print("Begin training...")
+model.fit(data, 0)
